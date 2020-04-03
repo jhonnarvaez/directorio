8237e3c0049ff464d66a5f062643b90f1f30dc49
jhonnarvaez <jhonnarvaez77@gmail.com>
2020-04-03 11:47:45 -0500

    commit de prueba

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 })-> name('inicio'); 
+
+Route::get('/login', function () {
+    return view('login');
+})-> name('login'); 
